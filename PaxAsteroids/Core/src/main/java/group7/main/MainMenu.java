e9749aadca99b17ffc33d0b2a925fa226ee1e0fd
Mathias Brix <mbrix17@student.sdu.dk>
2020-05-18 13:32:59 +0200

    New sprites on mainmenu

--- PaxAsteroids/Core/src/main/java/group7/main/MainMenu.java
+++ PaxAsteroids/Core/src/main/java/group7/main/MainMenu.java
@@ -12,6 +12,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
@@ -40,9 +41,11 @@ public class MainMenu implements Screen {
     private Texture backgound;
     private Skin skin;
     private Table table;
-    private Texture playButton, ExitButton;
+    private Texture playButton, ExitButton, Background;
     private BitmapFont white, black;
     private Label label;
+    Texture texture;
+    Sprite sprite;
     
 
 //    private static final int PlaybuttonX = 300;
@@ -53,9 +56,11 @@ public class MainMenu implements Screen {
 
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 1440, 800);
+        texture = new Texture("STUBBI.png");
+        sprite = new Sprite(texture);
+        playButton = new Texture("START.png");
+        ExitButton = new Texture("EXIT.png");
 
-        playButton = new Texture("PlayButton.png");
-        ExitButton = new Texture("QuitButton.png");
 
         
        // A drawable has information about its size and how to draw itself. It's used to determine size and position by ui components. 
@@ -64,15 +69,17 @@ public class MainMenu implements Screen {
        
         Drawable drawable = new TextureRegionDrawable(new TextureRegion(playButton));
         Drawable drawables = new TextureRegionDrawable(new TextureRegion(ExitButton));
+
         ImageButton Play = new ImageButton(drawable);
         ImageButton Exit = new ImageButton(drawables);
 
         Stage stage = new Stage(new ScreenViewport()); //Set up a stage for the ui  
         stage.addActor(Play);
         stage.addActor(Exit);
+        
 
         Gdx.input.setInputProcessor(stage);
-        Play.setPosition(500, 500);
+        Play.setPosition(613, 500);
         Play.addListener(new ClickListener() {
 
             @Override
@@ -84,11 +91,9 @@ public class MainMenu implements Screen {
             }
         });
 
-        
-        Exit.setPosition(500, 200);
+        Exit.setPosition(636, 300);
         Exit.addListener(new ClickListener()  {
          
-            
             @Override
             public void clicked(InputEvent event, float x, float y) {   
                 System.out.println("TJEK FOR EXIT");  
@@ -107,16 +112,18 @@ public class MainMenu implements Screen {
     @Override
     public void render(float f) {
         // Clear frame
-        Gdx.gl.glClearColor(100f / 255f, 100f / 255f, 250f / 255f, 1f);
+        
+        Gdx.gl.glClearColor(0f, 0f, 0f, 0f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         camera.update();
         MainMenu.batch.setProjectionMatrix(camera.combined);
 
         MainMenu.batch.begin();
+        MainMenu.batch.draw(sprite, 0, 0);
+        MainMenu.batch.draw(playButton, 613, 500);
 
-        MainMenu.batch.draw(playButton, 500, 500);
-
-        MainMenu.batch.draw(ExitButton, 500, 200);
+        MainMenu.batch.draw(ExitButton, 636, 300);
+        
 
         MainMenu.batch.end();
     }
